186891082f87b152a8d53f80574dcd77da1ade35
MasatoIkeda05 <2112136e@stu.kobe-u.ac.jp>
2024-06-15 18:35:11 +0900

    [add] Usersコントローラーの作成

---
+++ spec/system/users_spec.rbs_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSpec.rbs", type: :system do
+  #describe "ユーザー登録" do
+  #  it "正しい入力がされた時、新しいユーザーデータが作成される" do
+  #   fill_in "email", with: "testuser"
+  #   fill_in "password", with: "password"
+  #   fill_in "password_confirmation", with: "password"
+  #   click_button "新規登録"
+
+  #   expect {subject}.to change {User.count}.by(1)
+  #end
+  #end
+end
