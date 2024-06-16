be4a43f2de3ca13b74b3acd50a6994113f2f31d0
MasatoIkeda05 <2112136e@stu.kobe-u.ac.jp>
2024-06-16 16:07:31 +0900

    [add] ユーザー登録のE2Eテストを作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  #Set chrome not to open when executing system spec
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    end
+  end
 end
