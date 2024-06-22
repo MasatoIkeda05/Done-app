0b23b63f35cd152c9d7ba5e047faf865733cf76f
MasatoIkeda05 <2112136e@stu.kobe-u.ac.jp>
2024-06-22 21:40:22 +0900

    [refactor]　system specの可視性を向上

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "Users", type: :system do
     driven_by(:rack_test)
   end
 
-  describe "creating a new user" do
-    it "increases the user count by 1" do
-      visit new_user_path
+  describe "ユーザーの新規登録" do
+    it "正しい入力をした時、新しいユーザーが作成されることを確認する" do
+      visit sign_up_path
 
       fill_in "email", with: "test@example.com"
       fill_in "password", with: "password"
